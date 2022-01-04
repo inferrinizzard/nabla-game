6e7a3d234ec80feb6fadfcf306b7a72c5f66fcbe
inferrinizzard <inferrinizzard@gmail.com>
2022-01-03 16:21:55 -0800

    add remaining tests

--- tests/math.rs
+++ tests/math.rs
@@ -26,8 +26,9 @@ fn test_atomic_derivatives() {
     }
 }
 
+// test Add and Minus derivatives
 #[test]
-fn test_add_derivative() {
+fn test_add_derivatives() {
     // test first derivative
     assert_eq!(
         // dx(x + e^x)
@@ -74,5 +75,111 @@ fn test_add_derivative() {
             ),
             &Basis::BasisCard(BasisCard::One)
         )
+    );
+
+    // test trim 0
+    assert_eq!(
+        // dx(cos(x) + x)
+        derivative::derivative(&AddBasisNode(
+            &Basis::BasisCard(BasisCard::Cos),
+            &Basis::BasisCard(BasisCard::One),
+        )),
+        // sin(x)
+        Basis::BasisCard(BasisCard::Sin)
+    );
+}
+
+// test Mult and Div derivatives
+#[test]
+fn test_mult_derivatives() {
+    // test mult derivative
+    assert_eq!(
+        // dx(x^2 * cos(x))
+        derivative::derivative(&MultBasisNode(
+            &Basis::BasisCard(BasisCard::X2),
+            &Basis::BasisCard(BasisCard::Cos),
+        )),
+        //  x^2*sin(x) + cos(x)*x
+        AddBasisNode(
+            &MultBasisNode(
+                &Basis::BasisCard(BasisCard::X2),
+                &Basis::BasisCard(BasisCard::Sin),
+            ),
+            &MultBasisNode(
+                &Basis::BasisCard(BasisCard::Cos),
+                &Basis::BasisCard(BasisCard::X),
+            ),
+        )
+    );
+
+    // test trim 1
+    assert_eq!(
+        // dx(x * e^x)
+        derivative::derivative(&MultBasisNode(
+            &Basis::BasisCard(BasisCard::X),
+            &Basis::BasisCard(BasisCard::E),
+        )),
+        //  x*e^x + e^x
+        AddBasisNode(
+            &MultBasisNode(
+                &Basis::BasisCard(BasisCard::X),
+                &Basis::BasisCard(BasisCard::E),
+            ),
+            &Basis::BasisCard(BasisCard::E)
+        )
+    );
+
+    // test div derivative
+    assert_eq!(
+        // dx(cos(x) / e^x)
+        derivative::derivative(&DivBasisNode(
+            &Basis::BasisCard(BasisCard::Cos),
+            &Basis::BasisCard(BasisCard::E),
+        )),
+        //  x^2*sin(x) + cos(x)*x
+        DivBasisNode(
+            &MinusBasisNode(
+                &MultBasisNode(
+                    &Basis::BasisCard(BasisCard::E),
+                    &Basis::BasisCard(BasisCard::Sin),
+                ),
+                &MultBasisNode(
+                    &Basis::BasisCard(BasisCard::Cos),
+                    &Basis::BasisCard(BasisCard::E),
+                )
+            ),
+            &MultBasisNode(
+                &Basis::BasisCard(BasisCard::Cos),
+                &Basis::BasisCard(BasisCard::Cos),
+            ),
+        )
+    );
+}
+
+// test Pow and Sqrt derivatives
+#[test]
+fn test_exponent_derivatives() {
+    // test pow derivative
+    assert_eq!(
+        // dx(x^4)
+        derivative::derivative(&PowBasisNode(4, &Basis::BasisCard(BasisCard::X))),
+        // x^3
+        PowBasisNode(3, &Basis::BasisCard(BasisCard::X))
+    );
+
+    // test trim to X2
+    assert_eq!(
+        // dx(x^3)
+        derivative::derivative(&PowBasisNode(3, &Basis::BasisCard(BasisCard::X))),
+        // x^3
+        Basis::BasisCard(BasisCard::X2)
+    );
+
+    // test Sqrt derivative
+    assert_eq!(
+        // dx(x^1/2)
+        derivative::derivative(&SqrtBasisNode(1, &Basis::BasisCard(BasisCard::X))),
+        // x^-1/2
+        SqrtBasisNode(-1, &Basis::BasisCard(BasisCard::X))
     );
 }
