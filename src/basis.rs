a284b578ab85f0ba6b4056fc39834205d48ba88a
inferrinizzard <inferrinizzard@gmail.com>
2022-01-13 21:52:59 -0800

    add basic canvas drawing

--- src/basis.rs
+++ src/basis.rs
@@ -1,4 +1,5 @@
 use std::fmt;
+use std::string::ToString;
 
 use super::game::EnumStr;
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
-use wasm_bindgen::prelude::*;
-use web_sys::console;
+use js_sys::Array;
+use wasm_bindgen::{prelude::*, JsCast};
+use web_sys::*;
 
 pub mod basis;
 pub mod cards;
@@ -16,6 +17,11 @@ mod util;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+pub struct Vector2 {
+    x: f64,
+    y: f64,
+}
+
 // This is like the `main` function, except for JavaScript.
 #[wasm_bindgen(start)]
 pub fn main_js() -> Result<(), JsValue> {
@@ -24,12 +30,75 @@ pub fn main_js() -> Result<(), JsValue> {
     #[cfg(debug_assertions)]
     console_error_panic_hook::set_once();
 
-    // Your code goes here!
-    console::log_1(&JsValue::from_str("Hello world!"));
+    let window = web_sys::window().unwrap();
+    let document = window.document().unwrap();
+    let canvas: HtmlCanvasElement = document.get_element_by_id("canvas").unwrap().dyn_into()?;
 
+    let bounds = Vector2 {
+        x: f64::from(canvas.width()),
+        y: f64::from(canvas.height()),
+    };
+    let center = Vector2 {
+        x: bounds.x / 2.0,
+        y: bounds.y / 2.0,
+    };
+
+    let context = canvas
+        .get_context("2d")?
+        .unwrap()
+        .dyn_into::<CanvasRenderingContext2d>()?;
     let game = game::Game::new();
-
-    console::log_1(&JsValue::from_str(&format!("{:?}", &game)));
+    draw_field(&context, &center, &game.field);
 
     Ok(())
+}
+
+pub fn draw_field(
+    context: &CanvasRenderingContext2d,
+    center: &Vector2,
+    field: &[Option<basis::Basis>; 6],
+) {
+    let rect_height = 300.0;
+    let rect_width = 225.0;
+    let gutter = 50.0;
+
+    context.set_font("48px serif");
+
+    for (i, card) in field.iter().enumerate() {
+        if card.is_none() {
+            context
+                .set_line_dash(&JsValue::from(&Array::fill(
+                    &Array::new_with_length(2),
+                    &JsValue::from(10),
+                    0,
+                    2,
+                )))
+                .expect(format!("Cannot set line dash for {:?}", card).as_str());
+        } else {
+            context
+                .set_line_dash(&JsValue::from(&js_sys::Array::new()))
+                .expect(format!("Cannot set line dash for {:?}", card).as_str());
+        }
+
+        context.begin_path();
+        let card_pos = Vector2 {
+            x: center.x + (i.rem_euclid(3) as f64) * (rect_width + gutter)
+                - rect_width * 1.5
+                - gutter,
+            y: center.y + (i.rem_euclid(2) as f64) * (rect_height + gutter)
+                - rect_height
+                - gutter / 2.0,
+        };
+        context.stroke_rect(card_pos.x, card_pos.y, rect_width, rect_height);
+
+        if let Some(basis) = card {
+            context
+                .fill_text(
+                    &basis.to_string(),
+                    card_pos.x + rect_width / 2.0,
+                    card_pos.y + rect_width / 2.0,
+                )
+                .expect(&format!("Cannot print text for {:?}", card));
+        }
+    }
 }
