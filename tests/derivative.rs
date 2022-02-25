00a9d6fb87b32beca184496f6954a58f182c7a3a
inferrinizzard <inferrinizzard@gmail.com>
2022-02-25 06:58:58 -0800

    add logarithm derivative rests

--- tests/derivative.rs
+++ tests/derivative.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
 
 use nabla_game;
+use nabla_game::basis::builders::ACosBasisNode;
 use nabla_game::basis::structs::*;
 use nabla_game::math::derivative::derivative;
 
@@ -36,25 +37,25 @@ fn test_add_derivatives() {
     a = Basis::x() + e_x();
     b = Basis::from(1) + e_x();
     println!("d/dx({}) = {}", a, b);
-    assert_eq!(derivative(&a), b,);
+    assert_eq!(derivative(&a), b);
 
     // test second derivative
     a = cos_x() + (Basis::x() ^ 2);
     b = -cos_x() + Basis::from(2);
     println!("d/dx(d/dx({})) = {}", a, b);
-    assert_eq!(derivative(&derivative(&a)), b,);
+    assert_eq!(derivative(&derivative(&a)), b);
 
     // test trinomial (nested BasisNode)
     a = sin_x() + (Basis::x() ^ 2) + Basis::x();
     b = cos_x() + (Basis::x() * 2) + Basis::from(1);
     println!("d/dx({}) = {}", a, b);
-    assert_eq!(derivative(&a), b,);
+    assert_eq!(derivative(&a), b);
 
     // test trim 0
     a = cos_x() + Basis::from(1);
     b = -sin_x();
     println!("d/dx({}) = {}", a, b);
-    assert_eq!(derivative(&a), b,);
+    assert_eq!(derivative(&a), b);
 }
 
 // test Mult and Div derivatives
@@ -66,19 +67,19 @@ fn test_mult_derivatives() {
     a = (Basis::x() ^ 2) * cos_x();
     b = ((Basis::x() ^ 2) * -sin_x()) + (cos_x() * Basis::x() * 2);
     println!("d/dx({}) = {}", a, b);
-    assert_eq!(derivative(&a), b,);
+    assert_eq!(derivative(&a), b);
 
     // test trim 1
     a = Basis::x() * e_x();
     b = (Basis::x() * e_x()) + e_x();
     println!("d/dx({}) = {}", a, b);
-    assert_eq!(derivative(&a), b,);
+    assert_eq!(derivative(&a), b);
 
     // test div derivative
     a = cos_x() / e_x();
     b = ((-sin_x() * e_x()) - (cos_x() * e_x())) / (e_x() ^ 2);
     println!("d/dx({}) = {}", a, b);
-    assert_eq!(derivative(&a), b,);
+    assert_eq!(derivative(&a), b);
 }
 
 // test Pow and Sqrt derivatives
@@ -90,25 +91,47 @@ fn test_exponent_derivatives() {
     a = Basis::x() ^ 4;
     b = 4 * (Basis::x() ^ 3);
     println!("d/dx({}) = {}", a, b);
-    assert_eq!(derivative(&a), b,);
+    assert_eq!(derivative(&a), b);
 
     // test sqrt derivative
     a = Basis::x() ^ (1, 2);
     b = (Basis::x() ^ (-1, 2)) / 2;
     println!("d/dx({}) = {}", a, b);
-    assert_eq!(derivative(&a), b,);
+    assert_eq!(derivative(&a), b);
 
     // test sin pow derivative
     a = cos_x() ^ 2;
     b = -2 * sin_x() * cos_x();
     println!("d/dx({}) = {}", a, b);
-    assert_eq!(derivative(&a), b,);
+    assert_eq!(derivative(&a), b);
 
     // test e pow derivative
     a = e_x() ^ (1, 2);
     b = e(&(Basis::x() / 2)) / 2;
     println!("d/dx({}) = {}", a, b);
-    assert_eq!(derivative(&a), b,);
+    assert_eq!(derivative(&a), b);
 }
 
-// TODO: add tests for log
+// test Log derivatives
+#[test]
+fn test_log_derivatives() {
+    let (mut a, mut b);
+
+    // test log(x) derivative
+    a = log_x();
+    b = Basis::x() ^ -1;
+    println!("d/dx({}) = {}", a, b);
+    assert_eq!(derivative(&a), b);
+
+    // test log(sin(x)) derivative
+    a = log(&sin_x());
+    b = cos_x() / sin_x();
+    println!("d/dx({}) = {}", a, b);
+    assert_eq!(derivative(&a), b);
+
+    // test log(acos(x)) derivative
+    a = log(&ACosBasisNode(&Basis::x()));
+    b = derivative(&ACosBasisNode(&Basis::x())) / ACosBasisNode(&Basis::x());
+    println!("d/dx({}) = {}", a, b);
+    assert_eq!(derivative(&a), b);
+}
