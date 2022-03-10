5cdf4d18c01d6d75604f2534462d8e44f28a2491
inferrinizzard <inferrinizzard@gmail.com>
2022-03-09 22:33:05 -0800

    add liate operator Mult filters

--- src/math/liate.rs
+++ src/math/liate.rs
@@ -60,7 +60,7 @@ pub fn logarithmic(operator: BasisOperator, u: &Basis, dv: &Basis) -> Option<Bas
 }
 
 /// tries integration with an inverse trigonometric component, skips if doesn't match pattern
-pub fn inv_trig(_operator: BasisOperator, u: &Basis, dv: &Basis) -> Option<Basis> {
+pub fn inv_trig(_operator: BasisOperator, u: &Basis, _dv: &Basis) -> Option<Basis> {
     // current temp short circuit
     if u.is_node(BasisOperator::Acos) | u.is_node(BasisOperator::Asin) {
         // use FULL_COMPUTE here with x^n
@@ -82,25 +82,27 @@ pub fn inverse(operator: BasisOperator, u: &Basis, dv: &Basis) -> Option<Basis> 
 
 /// tries integration with a power component, skips if doesn't match pattern
 pub fn algebraic(operator: BasisOperator, u: &Basis, dv: &Basis) -> Option<Basis> {
-    match u {
-        Basis::BasisNode(BasisNode {
-            // any fractional exponent is not accepted
-            operator: BasisOperator::Pow(Fraction { n, d: 1 }),
-            ..
-        }) => {
-            let flag = unsafe { FULL_COMPUTE };
-            // skip if too complex
-            if flag || *n < 4 {
-                return Some(tabular_integration(u, dv));
+    if operator == BasisOperator::Mult {
+        match u {
+            Basis::BasisNode(BasisNode {
+                // any fractional exponent is not accepted
+                operator: BasisOperator::Pow(Fraction { n, d: 1 }),
+                ..
+            }) => {
+                let flag = unsafe { FULL_COMPUTE };
+                // skip if too complex
+                if flag || *n < 4 {
+                    return Some(tabular_integration(u, dv));
+                }
             }
+            Basis::BasisLeaf(BasisLeaf {
+                element: BasisElement::X,
+                ..
+            }) => {
+                return Some(integration_by_parts(u, dv));
+            }
+            _ => {}
         }
-        Basis::BasisLeaf(BasisLeaf {
-            element: BasisElement::X,
-            ..
-        }) => {
-            return Some(integration_by_parts(u, dv));
-        }
-        _ => {}
     }
 
     None
@@ -124,61 +126,71 @@ pub fn trig(operator: BasisOperator, u: &Basis, dv: &Basis) -> Option<Basis> {
         }
     }
 
-    // f(cos)sin | f(sin)cos
-    if let Basis::BasisNode(BasisNode {
-        operator: inner_operator,
-        operands: inner_operands,
-        ..
-    }) = u
-    {
-        let inner_base = &inner_operands[0];
-        if (inner_base.is_node(BasisOperator::Cos) && dv.is_node(BasisOperator::Sin))
-            || (inner_base.is_node(BasisOperator::Sin) && dv.is_node(BasisOperator::Cos))
+    if operator == BasisOperator::Mult {
+        // f(cos)sin | f(sin)cos
+        if let Basis::BasisNode(BasisNode {
+            operator: inner_operator,
+            operands: inner_operands,
+            ..
+        }) = u
         {
-            let sign = if inner_base.is_node(BasisOperator::Cos) {
-                -1
-            } else {
-                1
-            };
-            match inner_operator {
-                BasisOperator::Pow(Fraction { n, d: 1 }) => {
-                    return Some((inner_base.clone() ^ (n + 1)) * sign)
+            let inner_base = &inner_operands[0];
+            if (inner_base.is_node(BasisOperator::Cos) && dv.is_node(BasisOperator::Sin))
+                || (inner_base.is_node(BasisOperator::Sin) && dv.is_node(BasisOperator::Cos))
+            {
+                let sign = if inner_base.is_node(BasisOperator::Cos) {
+                    -1
+                } else {
+                    1
+                };
+                match inner_operator {
+                    BasisOperator::Pow(Fraction { n, d: 1 }) => {
+                        return Some((inner_base.clone() ^ (n + 1)) * sign)
+                    }
+                    BasisOperator::Log => {
+                        return Some(
+                            (inner_base.clone() * (LogBasisNode(inner_base) - Basis::from(1)))
+                                * sign,
+                        )
+                    }
+                    _ => {}
                 }
-                BasisOperator::Log => {
-                    return Some(
-                        (inner_base.clone() * (LogBasisNode(inner_base) - Basis::from(1))) * sign,
-                    )
-                }
-                _ => {}
             }
         }
     }
+
     None
 }
 
 /// tries integration with an exponential component, skips if doesn't match pattern
 pub fn exponential(operator: BasisOperator, u: &Basis, dv: &Basis) -> Option<Basis> {
-    // dv is e
-    if u.is_node(BasisOperator::Cos) {
-        return Some((dv.clone() * (CosBasisNode(&Basis::x()) + SinBasisNode(&Basis::x()))) / 2);
-    } else if u.is_node(BasisOperator::Sin) {
-        return Some((dv.clone() * (SinBasisNode(&Basis::x()) - CosBasisNode(&Basis::x()))) / 2);
-    }
+    if operator == BasisOperator::Mult {
+        // dv is e
+        if u.is_node(BasisOperator::Cos) {
+            return Some(
+                (dv.clone() * (CosBasisNode(&Basis::x()) + SinBasisNode(&Basis::x()))) / 2,
+            );
+        } else if u.is_node(BasisOperator::Sin) {
+            return Some(
+                (dv.clone() * (SinBasisNode(&Basis::x()) - CosBasisNode(&Basis::x()))) / 2,
+            );
+        }
 
-    if let Basis::BasisNode(BasisNode {
-        operator: BasisOperator::E,
-        operands: dv_operands,
-        ..
-    }) = dv
-    {
         if let Basis::BasisNode(BasisNode {
-            operator: BasisOperator::Pow(Fraction { n, d: 1 }),
+            operator: BasisOperator::E,
+            operands: dv_operands,
             ..
-        }) = dv_operands[0]
+        }) = dv
         {
-            // x^(n-1)e^(x^n)
-            if u.is_node(BasisOperator::Pow(Fraction { n: n - 1, d: 1 })) {
-                return Some(dv.clone() * u.coefficient() / n);
+            if let Basis::BasisNode(BasisNode {
+                operator: BasisOperator::Pow(Fraction { n, d: 1 }),
+                ..
+            }) = dv_operands[0]
+            {
+                // x^(n-1)e^(x^n)
+                if u.is_node(BasisOperator::Pow(Fraction { n: n - 1, d: 1 })) {
+                    return Some(dv.clone() * u.coefficient() / n);
+                }
             }
         }
     }
